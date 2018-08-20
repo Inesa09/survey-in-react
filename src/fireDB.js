7afb4e93ce4aa597e4f8541a7debb065e041fbdd
Inesa09 <inesusja@gmail.com>
2018-08-20 14:46:03 +0300

    added firebase

---
+++ src/fireDB.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase'
+
+var config = {
+    apiKey: "AIzaSyBq9NBrnLFLkjUXlVNWMzV55XYGDLjY3JE",
+    authDomain: "survey-in-react.firebaseapp.com",
+    databaseURL: "https://survey-in-react.firebaseio.com",
+    projectId: "survey-in-react",
+    storageBucket: "survey-in-react.appspot.com",
+    messagingSenderId: "62317409113"
+};
+var fireDB = firebase.initializeApp(config);
+
+export default fireDB;
