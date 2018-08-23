cbf6dbf2b84947097ac8ccd279c368f9803220a1
Inesa09 <inesusja@gmail.com>
2018-08-24 00:43:00 +0300

    final commit

--- src/containers/App.js
+++ src/containers/App.js
@@ -10,110 +10,109 @@ class App extends Component {
 
   constructor(props) {
     super(props);
-    this.state = { 
-        post: 0,
-        text: [],
-        previosIndexList :[],
-        show: 0
+    this.state = {
+      post: 0,
+      text: [],
+      previosIndexList: [],
+      show: 0
     }; // <- set up react state
   }
 
   showPrev = (e) => {
     e.preventDefault();
     let temporaryList = this.state.previosIndexList;
-    if(temporaryList.length > 0){
-    let previosElement = temporaryList.pop();
-    this.setState({post : previosElement, previosIndexList : temporaryList});
-    this.scrollToTop();
+    if (temporaryList.length > 0) {
+      let previosElement = temporaryList.pop();
+      this.setState({ post: previosElement, previosIndexList: temporaryList });
+      this.scrollToTop();
     }
   }
- toUndef = (post,e) =>{
-    e.preventDefault();
-  console.log(this.state.previosIndexList);
-  let temporaryList = this.state.previosIndexList;
-  temporaryList.push(post);
-  this.setState({post : undefined, previosIndexList : temporaryList});
- }
+
   showNext = (post, e) => {
     e.preventDefault();
     let temporaryList = this.state.previosIndexList;
     let number = this.findNextUnsubmitedElement(post);
     console.log(this.state.previosIndexList);
-    if(number != undefined) {
+    if (number != undefined) {
       temporaryList.push(post);
-      this.setState({post : number, previosIndexList : temporaryList});
+      this.setState({ post: number, previosIndexList: temporaryList });
       this.scrollToTop();
-    } 
-    // else
-    //   this.setState({post: undefined});
-    // } 
-  }
-
-  findNextUnsubmitedElement= (post) => {
-    for(let i = post + 1, size = Object.values(this.state.text).length; i < size; i++){
-        if(this.state.text[i][3].length === 0){
-          return i;
-        }
     }
   }
 
-  componentDidMount() { 
-    fireDB.database().ref('masterSheet/').on('value', snapshot => { 
-      this.setState({text : snapshot.val()});
-    }); 
-  } 
+  toUndef = (post, e) => {
+    e.preventDefault();
+    console.log(this.state.previosIndexList);
+    let temporaryList = this.state.previosIndexList;
+    temporaryList.push(post);
+    this.setState({ post: undefined, previosIndexList: temporaryList });
+  }
+
+  findNextUnsubmitedElement = (post) => {
+    for (let i = post + 1, size = Object.values(this.state.text).length; i < size; i++) {
+      if (this.state.text[i][3].length === 0) {
+        return i;
+      }
+    }
+  }
 
   scrollToTop = () => {
-    try{
-      document.getElementById("form").reset(); 
+    try {
+      document.getElementById("form").reset();
       document.getElementById('top').scrollIntoView(true);
-    } catch (err){ }
+    } catch (err) { }
+  }
+
+  componentDidMount() {
+    fireDB.database().ref('masterSheet/').on('value', snapshot => {
+      this.setState({ text: snapshot.val() });
+    });
   }
 
   render() {
-    const { post, text} = this.state;
+    const { post, text } = this.state;
     let number = this.findNextUnsubmitedElement(post);
-    if(post != 0){
+    if (post != 0) {
       number = post;
     }
     let isNextElementExist = this.findNextUnsubmitedElement(number) != undefined;
 
 
-    if(text.length === 0)  //Loading
+    if (text.length === 0)  //Loading
       return (
         <Top>
           <Message color='teal' icon='circle notched loading icon'
-            text1='Just one second' text2='We are fetching that content for you.'/>
+            text1='Just one second' text2='We are fetching that content for you.' />
         </Top>
       )
     else if (post === undefined || (post === 0 && number === undefined)) //All text submitted
       return (
         <Top>
           <Message color='green' icon='check icon'
-            text1='Sorry' text2='All of the texts are already reviewed.'/>
-          <button className = {this.state.previosIndexList.length > 0 ? 
+            text1='Sorry' text2='All of the texts are already reviewed.' />
+          <button className={this.state.previosIndexList.length > 0 ?
             'ui left animated violet basic massive button' : 'ui grey basic massive button'}
-                style= {{margin: '30px 33%' }}
-                onClick={this.showPrev}>
-                <div className='visible content'> Previous Text</div>
-                <div className='hidden content'>
-                    <i aria-hidden='true' 
-                    className={this.state.previosIndexList.length > 0 ? 'arrow left icon' : ''} />
-                </div>
+            style={{ margin: '30px 33%' }}
+            onClick={this.showPrev}>
+            <div className='visible content'> Previous Text</div>
+            <div className='hidden content'>
+              <i aria-hidden='true'
+                className={this.state.previosIndexList.length > 0 ? 'arrow left icon' : ''} />
+            </div>
           </button>
         </Top>
       )
-    else if(number !=undefined  && text.length != 0) //Main
+    else if (number != undefined && text.length != 0) //Main
       return (
         <Top>
-         <Heading heading = {`Post Content - Related to ${text[number][1]}`}/>
-          <Text text={text[number][2]}/>
+          <Heading heading={`Post Content - Related to ${text[number][1]}`} />
+          <Text text={text[number][2]} />
           <Heading heading={"Post Review (Your input)"} />
-          <Survey post={number} 
-             showPrev={this.showPrev} showNext={this.showNext}
-             numberOfPreviousElemnts={this.state.previosIndexList.length} 
-             nextElementExistanse ={isNextElementExist}
-             toUndef = {this.toUndef}/>
+          <Survey post={number}
+            showPrev={this.showPrev} showNext={this.showNext}
+            numberOfPreviousElemnts={this.state.previosIndexList.length}
+            nextElementExistanse={isNextElementExist}
+            toUndef={this.toUndef} />
         </Top>
       )
   }
--- src/containers/Survey.js
+++ src/containers/Survey.js
@@ -7,179 +7,178 @@ import RadioWithInput from '../components/RadioWithInput';
 
 class Survey extends Component {
 
-    constructor(props) {
-        super(props);
-        this.state = { 
-            questions: ['empty',
-                'How relevant is the post to the place?',
-                'How interesting is the post?',
-                'Summary of the post',
-                'Trivia Question About it #1',
-                'Trivia Question About it #1',
-                'Trivia Question About it #2',
-                'Trivia Question About it #2',
-                'Free Notes'],
-            answers: {},
-            addAnswers: this.addAnswers,
-        }; // <- set up react state
+  constructor(props) {
+    super(props);
+    this.state = {
+      questions: ['empty',
+        'How relevant is the post to the place?',
+        'How interesting is the post?',
+        'Summary of the post',
+        'Trivia Question About it #1',
+        'Trivia Question About it #1',
+        'Trivia Question About it #2',
+        'Trivia Question About it #2',
+        'Free Notes'],
+      answers: {},
+    }; // <- set up react state
+  }
+
+  handleAnswer = (question, e) => {
+    let copy = this.state.answers;
+    copy[question + 2] = e.target.value;
+    this.setState({ answers: copy });
+  }
+
+  //Submit
+  addAnswers = (e, post) => {
+    var size = Object.keys(this.state.answers).length;
+    if (size !== 8)
+      this.showEl('error');
+    else {
+      e.preventDefault(); // <- prevent form submit from reloading the page
+
+      if (this.props.nextElementExistanse)
+        this.props.showNext(post, e);
+      else
+        this.props.toUndef(post, e);
+
+      fireDB.database().ref(`masterSheet/${post}`).update(this.state.answers);
+      document.getElementById("form").reset(); // <- clear the input
+      this.setState({ answers: {} }); // <- clear the state
+      this.showEl('success');
     }
+  }
 
-    handleAnswer = (question, e) => {
-        let copy = this.state.answers;
-        copy[question+2] = e.target.value;
-        this.setState({answers: copy});
+  //CSS methods
+  showEl = (el) => {
+    if (document.getElementById('error').style.display === 'none') { //to avoid two messages when error in addMessage
+      const current = document.getElementById(el);
+      current.style.display = 'block';
+      current.scrollIntoView(true);
+      setTimeout(this.hideEl, 2000, el);
     }
+  }
 
-    //Submit
-    addAnswers = (e, post) => {
-        var size = Object.keys(this.state.answers).length;
-        if( size !== 8)
-            this.showEl('error');
-        else {
-            e.preventDefault(); // <- prevent form submit from reloading the page
-
-        if(this.props.nextElementExistanse)
-            this.props.showNext(post, e);
-        else
-            this.props.toUndef(post, e);
-
-        fireDB.database().ref(`masterSheet/${post}`).update(this.state.answers);
-        document.getElementById("form").reset(); // <- clear the input
-        this.setState({answers: {}}); // <- clear the state
-        this.showEl('success');
-        }
+  hideEl = (el) => {
+    try {
+      document.getElementById(el).style.display = 'none';
+    } catch (err) {
     }
+  }
 
-    //CSS methods
-    showEl = (el) => {
-        if (document.getElementById('error').style.display === 'none'){ //to avoid two messages when error in addMessage
-            const current = document.getElementById(el);
-            current.style.display = 'block';
-            current.scrollIntoView(true);
-            setTimeout(this.hideEl, 2000, el);
-        }
-    }
+  submitBtnHover = (color) => {
+    document.getElementById('submitBtn').style.backgroundColor = color;
+  }
 
-    hideEl = (el) => {
-        try{
-            document.getElementById(el).style.display = 'none';
-        } catch(err){
-        }
-    }
+  render() {
+    // for(let i=3; i <=629;i++)
+    // fireDB.database().ref(`masterSheet/${i}`).remove();
+    const { questions, answers } = this.state;
+    const { post, numberOfPreviousElemnts, nextElementExistanse, showNext, showPrev } = this.props;
+    return (
+      <div className="Survey">
+        <form id='form'>
+          <Radio
+            question={questions[1]}
+            handleOptionChange={(e) => this.handleAnswer(1, e)}
+          />
+          <Radio
+            question={questions[2]}
+            handleOptionChange={(e) => this.handleAnswer(2, e)}
+          />
+          <Input
+            question={questions[3]}
+            handleTextInput={(e) => this.handleAnswer(3, e)}
+            value={answers['5']}
+          />
 
-    submitBtnHover = (color) => {
-        document.getElementById('submitBtn').style.backgroundColor = color;
-    }
+          <Input
+            question={questions[4]}
+            tooltip={'answer is..'}
+            handleTextInput={(e) => this.handleAnswer(4, e)}
+            value={answers['6']}
+          />
+          <RadioWithInput
+            question={questions[5]}
+            answer={'Default'}
+            tooltip={'answer is..'}
+            handleAnswer={(e) => this.handleAnswer(5, e)}
+            value={answers['7']}
+          />
 
-    render() {
-        // for(let i=3; i <=629;i++)
-        // fireDB.database().ref(`masterSheet/${i}`).remove();
-        const {questions, answers} = this.state;
-        const {post, numberOfPreviousElemnts, nextElementExistanse, showNext, showPrev} = this.props;
-        return (
-            <div className="Survey">
-                <form id='form'>
-                    <Radio
-                    question={questions[1]} 
-                    handleOptionChange={(e) => this.handleAnswer(1, e)} 
-                    />
-                    <Radio
-                    question={questions[2]} 
-                    handleOptionChange={(e) => this.handleAnswer(2, e)} 
-                    />
-                    <Input 
-                    question={questions[3]}
-                    handleTextInput={(e) => this.handleAnswer(3, e)}
-                    value={answers['5']}
-                    />
+          <Input
+            question={questions[6]}
+            tooltip={'answer is..'}
+            handleTextInput={(e) => this.handleAnswer(6, e)}
+            value={answers['8']}
+          />
+          <RadioWithInput
+            question={questions[7]}
+            answer={'Default'}
+            tooltip={'answer is..'}
+            handleAnswer={(e) => this.handleAnswer(7, e)}
+            value={answers['9']}
+          />
 
-                    <Input 
-                    question={questions[4]}
-                    tooltip={'answer is..'}
-                    handleTextInput={(e) => this.handleAnswer(4, e)}
-                    value={answers['6']}
-                    />
-                    <RadioWithInput 
-                    question={questions[5]}
-                    answer={'Default'}
-                    tooltip={'answer is..'}
-                    handleAnswer={(e) => this.handleAnswer(5, e)}
-                    value={answers['7']}
-                    />
-
-                    <Input 
-                    question={questions[6]}
-                    tooltip={'answer is..'}
-                    handleTextInput={(e) => this.handleAnswer(6, e)}
-                    value={answers['8']}
-                    />
-                    <RadioWithInput 
-                    question={questions[7]}
-                    answer={'Default'}
-                    tooltip={'answer is..'}
-                    handleAnswer={(e) => this.handleAnswer(7, e)}
-                    value={answers['9']}
-                    />
-
-                    <Input 
-                    question={questions[8]}
-                    handleTextInput={(e) => this.handleAnswer(8, e)}
-                    value={answers['10']}
-                    />
+          <Input
+            question={questions[8]}
+            handleTextInput={(e) => this.handleAnswer(8, e)}
+            value={answers['10']}
+          />
 
 
-                    <div style={{
-                        display: 'flex',
-                        justifyContent: 'center',
-                    }}>
-                        <button className='ui big button' id='submitBtn'
-                            style={{    
-                                backgroundColor: 'rgb(109, 97, 136)',
-                                color: 'white',
-                                marginTop: '20px',
-                            }}
-                            onClick={(e) => { this.addAnswers(e, post)}}
-                            onMouseOver={() => this.submitBtnHover('rgb(58, 46, 87)')}
-                            onMouseOut={() => this.submitBtnHover('rgb(109, 97, 136)')}> 
-                            Submit 
+          <div style={{
+            display: 'flex',
+            justifyContent: 'center',
+          }}>
+            <button className='ui big button' id='submitBtn'
+              style={{
+                backgroundColor: 'rgb(109, 97, 136)',
+                color: 'white',
+                marginTop: '20px',
+              }}
+              onClick={(e) => { this.addAnswers(e, post) }}
+              onMouseOver={() => this.submitBtnHover('rgb(58, 46, 87)')}
+              onMouseOut={() => this.submitBtnHover('rgb(109, 97, 136)')}>
+              Submit
                         </button>
-                    </div>
+          </div>
 
-                    <div style={{
-                        display: 'flex',
-                        justifyContent: 'space-between',
-                    }}>
-                        <button className = {numberOfPreviousElemnts > 0 ? 
-                        'ui left animated violet basic button' : 'ui grey basic button'}
-                            style= {{margin: '30px 50px'}}
-                            onClick={showPrev}>
-                            <div className='visible content'> Previous Text</div>
-                            <div className='hidden content'>
-                                <i aria-hidden='true' 
-                                className={numberOfPreviousElemnts > 0 ? 'arrow left icon' : ''} />
-                            </div>
-                        </button>
-                        <button className= {nextElementExistanse ? 
-                        'ui animated violet basic button' : 'ui grey basic button'}
-                            style={{margin: '30px 50px'}}
-                            onClick={(e) => showNext(post, e) }>
-                            <div className='visible content'>Next Text</div>
-                            <div className='hidden content'>
-                                <i aria-hidden='true' 
-                                className={nextElementExistanse > 0 ? 'arrow right icon' : ''} />
-                            </div>
-                        </button>
-                    </div>
+          <div style={{
+            display: 'flex',
+            justifyContent: 'space-between',
+          }}>
+            <button className={numberOfPreviousElemnts > 0 ?
+              'ui left animated violet basic button' : 'ui grey basic button'}
+              style={{ margin: '30px 50px' }}
+              onClick={showPrev}>
+              <div className='visible content'> Previous Text</div>
+              <div className='hidden content'>
+                <i aria-hidden='true'
+                  className={numberOfPreviousElemnts > 0 ? 'arrow left icon' : ''} />
+              </div>
+            </button>
+            <button className={nextElementExistanse ?
+              'ui animated violet basic button' : 'ui grey basic button'}
+              style={{ margin: '30px 50px' }}
+              onClick={(e) => showNext(post, e)}>
+              <div className='visible content'>Next Text</div>
+              <div className='hidden content'>
+                <i aria-hidden='true'
+                  className={nextElementExistanse > 0 ? 'arrow right icon' : ''} />
+              </div>
+            </button>
+          </div>
 
 
-                    <SmallMessage name='success' text1='Form Completed'
-                        text2='You have saved your answers.' />
-                    <SmallMessage name='error' text1='Action Forbidden'
-                        text2='You have to answer on all questions.' />
-                </form>
-            </div>
-        )
-    }
+          <SmallMessage name='success' text1='Form Completed'
+            text2='You have saved your answers.' />
+          <SmallMessage name='error' text1='Action Forbidden'
+            text2='You have to answer on all questions.' />
+        </form>
+      </div>
+    )
+  }
 }
 
 export default Survey;
