68361f72072b9349c97c2a74e9da3a4d78355ae5
Inesa09 <inesusja@gmail.com>
2019-01-25 00:27:21 +0200

    Revert "Revert "change view of img upload""
    This reverts commit d6008175babd1039a099f2fe0611b4e64626fd00.

--- src/components/MapContainer.js
+++ src/components/MapContainer.js
@@ -182,11 +182,11 @@ const MapContainer = compose(
             }}
           />
         </SearchBox>
-        <div class="ui buttons" style={{ display: 'flex', justifyContent: 'center', 
+        <div className="ui buttons" style={{ display: 'flex', justifyContent: 'center', 
           margin: '20px 30px 20px 20px' }}>
-          <div class="ui button active" id='btnG' onClick={props.clickGoogle} > מיקום ב </div >
-          <div class="or"></div>
-          <div  class="ui button violet" id='btnC' onClick={props.clickCustom} > מאגר פנימי </div >
+          <div className="ui button active" id='btnG' onClick={props.clickGoogle} > מיקום ב </div >
+          <div className="or"></div>
+          <div  className="ui button violet" id='btnC' onClick={props.clickCustom} > מאגר פנימי </div >
         </div>
 
         <div id='inputC' >
